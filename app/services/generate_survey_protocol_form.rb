a44bbefca39bfcf34b9f3b9d6ec10854fe22e9d5
Brian Glusman <brian@stellaservice.com>
2016-06-18 16:18:29 -0400

    working reform basic generation

--- app/services/generate_survey_protocol_form.rb
+++ app/services/generate_survey_protocol_form.rb
@@ -2,27 +2,36 @@ class GenerateSurveyProtocolForm
   attr_reader :fields
 
   def initialize(format)
-    @fields = format["fields"]
+    @fields = format['fields']
   end
 
   def each_field(&block)
-    fields.map{|f| Field.new(f) }.each(&block)
+    fields.map { |f| Field.new(f) }.each(&block)
   end
 
   def form_class
-    @form_class ||= Class.new(EntryForm) do
+    @form_class ||= define_form_class
+  end
+
+private
+
+  def define_form_class
+    Class.new(EntryForm).tap do |klass| 
+      each_field.each do |f|
+        klass.send :property, f.name, virtual: true
+      end
     end
   end
+
 
   class Field
     attr_reader :label, :type, :required, :options, :name
     def initialize(data)
-      @label    = data["label"]
-      @type     = data["field_type"]
-      @required = data["required"]
-      @options  = data["options"]
-      @name     = data["cid"]
+      @label    = data['label']
+      @type     = data['field_type']
+      @required = data['required']
+      @options  = data['options']
+      @name     = data['cid']
     end
   end
-
 end
