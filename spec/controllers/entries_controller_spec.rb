e0720709f67df3aef738576c67c21b78221d6b25
Chris Flipse <cflipse@gmali.com>
2016-06-19 12:26:51 -0400

    fix syntax error

--- spec/controllers/entries_controller_spec.rb
+++ spec/controllers/entries_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe EntriesController, type: :controller do
     @property = Property.create(name: "Test Property")
     @site = Site.create(name: "Test Site", property: @property)
     @protocol = SurveyProtocol.create(title: "Bees!")
-    @observation = Observation.create(protocol: @protocol, site_id: = @site.id)
+    @observation = Observation.create(protocol: @protocol, site_id: @site.id)
   end
 
 end
