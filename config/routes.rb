d1858f0354c86f25c6408490eb25d4bab24b8e09
Daniel P. Clark <6ftdan@gmail.com>
2016-06-20 13:41:26 -0400

    Now actually clean ;-)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-  root to: 'dashboard#show'
-
   resources :survey_protocols, only: [:new, :edit, :create, :update, :index] do
     resource :entry_fields, only: [:edit, :update]
   end
